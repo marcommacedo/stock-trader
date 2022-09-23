59c654f9fbd7123014f1e1d6051925774da1a09b
Marco Vinicius <marcoviniciusmoreiramacedo@gmail.com>
2022-09-22 22:41:13 -0300

    Carregando dados do firebase

--- src/Components/Header.vue
+++ src/Components/Header.vue
@@ -23,7 +23,7 @@
             <v-list-item @click="saveData">
               <v-list-item-title>Salvar Dados</v-list-item-title>
             </v-list-item>
-            <v-list-item>
+            <v-list-item @click="loadDataLocal">
               <v-list-item-title>Carregar Dados</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -48,13 +48,16 @@ export default {
     },
   },
   methods: {
-    ...mapActions(["randomizeStocks"]),
+    ...mapActions(["randomizeStocks", "loadData"]),
     endDay() {
       this.randomizeStocks();
     },
     saveData() {
       const { funds, stockPortfolio, stocks } = this.$store.getters;
       this.$http.put("data.json", { funds, stockPortfolio, stocks });
+    },
+    loadDataLocal() {
+      this.loadData();
     },
   },
 };
