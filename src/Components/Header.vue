91d45e92839de736a12b4d759de96ce721fbda20
Marco Vinicius <marcoviniciusmoreiramacedo@gmail.com>
2022-09-22 22:09:38 -0300

    Add finalizar dia

--- src/Components/Header.vue
+++ src/Components/Header.vue
@@ -14,7 +14,7 @@
       <v-spacer></v-spacer>
 
       <v-toolbar-items>
-        <v-btn text>Finalizar Dia</v-btn>
+        <v-btn text @click="endDay">Finalizar Dia</v-btn>
         <v-menu offset-y>
           <template v-slot:activator="{ on }">
             <v-btn text v-on="on">Salvar & Carregar</v-btn>
@@ -39,10 +39,18 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   computed: {
     funds() {
       return this.$store.getters.funds;
+    },
+  },
+  methods: {
+    ...mapActions(["randomizeStocks"]),
+    endDay() {
+      this.randomizeStocks();
     },
   },
 };
